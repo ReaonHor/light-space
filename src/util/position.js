13a8ff2099cdd3afc2edc6506d2d57bef4030ee2
ReasonHor <ReasonHor@163.com>
2023-04-27 14:47:18 +0800

    将translate改为translate3d，使用硬件加速

--- src/util/position.js
+++ src/util/position.js
@@ -1,10 +1,10 @@
 export function position (lo, block) {
   let windowWidth = document.documentElement.clientWidth
   if (windowWidth <= 700) {
-    return `transform: translate(0px,${lo * 100}%) scale(${block ? "1,1" : "0.001,0.001"
+    return `transform: translate3d(0px,${lo * 100}%,0) scale(${block ? "1,1" : "0.001,0.001"
       })`
   }
-  return `transform: translate(${lo % 3 * 100}%,${Math.floor(lo / 3) * 100}%) scale(${block ? "1,1" : "0.001,0.001"
+  return `transform: translate3d(${lo % 3 * 100}%,${Math.floor(lo / 3) * 100}%,0) scale(${block ? "1,1" : "0.001,0.001"
     })`
 }
 
